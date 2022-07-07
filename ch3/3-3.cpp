5ffd67df6377e8d0c7b5341f3f9f416f984420ba
josh930908 <joshdai930908@gmail.com>
2022-07-07 15:44:14 +0800

    7/7,ubuntu

--- ch3/3-3.cpp
+++ ch3/3-3.cpp
@@ -1,1 +1,18 @@
-//這題過於噁心，剛吃完早餐不想寫，不然可能會吐
+#include<bits/stdc++.h>
+using namespace std;
+
+int tower(int n,char from,char to,char mid){
+    if(n==1){
+        cout<<"ring 1 : "<<from<<" => "<<to<<'\n';
+        return;
+    }
+    if(n%3==0){
+        tower(n-1,from,mid,to);
+        cout<<"ring "<<n<<" : "<<from<<" => "<<to<<'\n';
+        tower(n-1,mid,to,from);
+    }
+}
+
+int main(){
+
+}
--- ch3/3-4.cpp
+++ ch3/3-4.cpp
@@ -1,31 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int k,target,ok;
-int pre[65537],num[5];
+int sq[65536],num[4];
+bool sol;
+//其實這一題就是枚舉，我也懶得再重新寫，直接拿去年AC的code來用...
+//遞迴就夠噁心了，還來枚舉...
+//這題又沒有說數字範圍，暈...
 
-inline void init(){
-    for(int i=1;i<=65536;++i)pre[i]=i*i;
+void pick(int remain,int id){
+	if(id==3){
+		int r=sqrt(remain);
+		if(r*r==remain){
+			num[3]=r;
+			sol=1;
+		}
+		return;
+	}
+	int l,u;
+	
+	if(!id)l=1;
+	else l=num[id-1];
+	u=sqrt(remain/(4-id));
+	
+	for(int i=l;i<=u;i++){
+		num[id]=i;
+		pick(remain-sq[i],id+1);
+	}
 }
 
-inline void pick(int id,int rest){
-    if(id==3){
-        int tmp=sqrt(rest);
-        if(pre[tmp]==rest){
-            for(int i=1;i<=4;++i){
-                ok=1;
-                cout<<num[i]<<" \n"[i==4];
-                return;
-            }
-        }
-        return;
-    }
-
-}
 
 int main(){
-    ios::sync_with_stdio(0),cin.tie(0);
-    cin >>k;target=1<<k;
-    pick(1,target);
-    if(!ok)cout<<0<<'\n';    
+	int n=1,i;
+	for(i=1;i<=65536;i++)sq[i]=i*i;
+	int k;
+	cin >>k;
+	
+	for(i=0;i<k;i++)n*=2;
+	
+	sol=0;
+	pick(n,0);
+	
+	if(sol){
+		for(i=0;i<4;i++)cout<<num[i]<<" ";
+	}else cout<<0;
+	
+	cout<<'\n';
 }
