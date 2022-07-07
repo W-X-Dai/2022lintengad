c988be77d76770beee2ab01fdef16744c6d60f2a
josh930908 <joshdai930908@gmail.com>
2022-07-07 18:21:19 +0800

    7/7,ubuntu

--- ch3/3-3.cpp
+++ ch3/3-3.cpp
@@ -1,18 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int tower(int n,char from,char to,char mid){
-    if(n==1){
-        cout<<"ring 1 : "<<from<<" => "<<to<<'\n';
-        return;
-    }
-    if(n%3==0){
-        tower(n-1,from,mid,to);
-        cout<<"ring "<<n<<" : "<<from<<" => "<<to<<'\n';
-        tower(n-1,mid,to,from);
-    }
+int cnt;
+//基本參考：https://hackmd.io/@apcsd3d4/r1JtpJFiI#/
+inline void hanio(int n,char from,char to,char mid){
+//    ++cnt;
+    if(n<=0)return;
+    hanio(n-1,from,mid,to);
+    ++cnt;
+    cout<<"ring "<<n<<" : "<<from<<" => "<<to<<'\n';
+    hanio(n-1,mid,to,from);
+}
+
+void distribute(int n,char from,char to,char mid){
+    if(n<=0)return;
+    
+    hanio(n-1,from,mid,to);
+    cout<<"ring "<<n<<" : "<<from<<" => "<<to<<'\n';
+    ++cnt;
+    distribute(n-2,mid,from,to);
 }
 
 int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n;
 
+    cin >>n;
+    // for(int i=0;i<n;i+=3){
+    //     hanio(n-i-1,'A','B','C');cout<<'\n';
+    //     ++cnt;
+    //     cout<<"ring "<<n-i<<" : A => C"<<'\n';
+    //     hanio(n-2-i,'B','A','C');cout<<'\n';
+    // }    
+    distribute(n,'A','C','B');
+
+
+    cout<<"共需"<<cnt<<"個移動"<<'\n';
 }
