0333e2b544a1dc7549508698d6f187f628ab1262
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-07-26 15:09:32 +0800

    ch14 added

--- ch14/14-1.cpp
+++ ch14/14-1.cpp
@@ -1,27 +1,26 @@
 #include<bits/stdc++.h>
-#define int long long
-#define pii pair<int,int>
+#define pii pair<double,double>
 #define x first
 #define y second
 using namespace std;
 
-int n,inf=0x3f3f3f3f3f3f3f3f,tmp[200005];
-pii arr[200005];
+int n,inf=2147483647,tmp[10005];
+pii arr[10005];
 
 inline bool cmp(int a,int b){
     return arr[a].y<arr[b].y;
 }
 
-inline int dist(pii a,pii b){
-    return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
+inline double dist(pii a,pii b){
+    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 }
 
-inline int divi(int l,int r){
+inline double divi(int l,int r){
     if(l==r)return inf;
     if(r-l==1)return dist(arr[l],arr[r]);
 
     int mid=(l+r)>>1;
-    int ans=min(divi(l,mid),divi(mid+1,r));
+    double ans=min(divi(l,mid),divi(mid+1,r));
 
     int cnt=0;
     for(int i=l;i<=r;++i){
@@ -30,10 +29,10 @@ inline int divi(int l,int r){
         }
     }
     
-    sort(tmp+1,tmp+1+cnt,cmp);
+//    sort(tmp+1,tmp+1+cnt,cmp);
     for(int i=1;i<=cnt;++i){
         for(int j=i+1;j<=cnt;++j){
-            if(arr[tmp[j]].y>arr[tmp[i]].y+ans)break;
+//            if(arr[tmp[j]].y>arr[tmp[i]].y+ans)break;
             ans=min(ans,dist(arr[tmp[j]],arr[tmp[i]]));
         }
     }
@@ -47,11 +46,15 @@ inline void sol(){
     sort(arr,arr+n,[](pii a,pii b){
         return a.x<b.x;
     });
-    cout<<divi(0,n-1)<<'\n';
+
+    double ans=divi(0,n-1);
+    
+    if(ans>10000)cout<<"INFINITY\n";
+    else cout<<setprecision(4)<<fixed<<ans<<'\n';
 }
 
-signed main(){
+int main(){
     ios::sync_with_stdio(0),cin.tie(0);
-    cin >>n;sol();
+    while(cin >>n && n)sol();
     return 0;
 }
